25c25bb8054e8e9515d3cb736678503f4802b3c4
Glitch (shopbot3) <none>
2019-04-25 15:56:24 +0000

    🏡🚅 Updated with Glitch

--- events/guildMemberAdd.js
+++
@@ -1,5 +0,0 @@
-module.exports = member => {
-    let username = member.user.username;
-    member.sendMessage('https://giphy.com/gifs/animation-cool-hello-xUPGGDNsLvqsBOhuU0 Selamlar,Değerli **' + username + '** Discord Sunucuma Hoşgeldin :kissing_heart: #kurallar a bakmayı unutma,lazım olacağından eminim. :thinking: Keyifli Vakit Geçirmen Dileğiyle Server Kurucusu ve bot yapımcısı DraqOns. https://gph.is/2VhWm6k :wink:');
-
-};
